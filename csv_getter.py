b80d86374f4bb1a8ee0c214d9fafae81083d5562
ashrdev <ashrdev@gmail.com>
2023-01-06 22:19:46 -0500

    Update csv_getter.py

--- csv_getter.py
+++ csv_getter.py
@@ -1,8 +1,8 @@
 #Tool to help get .csv file for ETH-USD data that we can use for backtesting
 import csv
 from binance.client import Client
-api_key = "Qe9Im0C47NqNaU52Bs4hQsTrWoryROEU2L40T2YaxrUAPXr5A5tgySSUmmPm9gYY" #CREATE NEW API KEY IN THE FUTURE
-secret_key = "rhMQ8mTcte4G7axxT9SIeWiD1qAF8pudE7MCCogBTgcFPgMjAePJwm3isjg0fxDg" #VERY IMPORTANT
+api_key = "my_api_key" #Replace with actual api key
+secret_key = "my_secret_api_key" #Replace with actual secret api key
 
 def get_data(start_date, end_date, month, year):
     client = Client(api_key, secret_key, tld='us')
